5d651cb95824a4b0f375ddca7182cb5a8a89c090
viperior <viperior@gmail.com>
2020-02-02 23:24:48 +0000

    Add optmimization method that removes impossible candidates from the original list before processing.

--- two-sum-problem.py
+++ two-sum-problem.py
@@ -12,9 +12,18 @@ class Solution:
             nums_dict_list.append(current_num_dict)
             current_num_index += 1
             
+        original_list_element_count = len(nums_dict_list)
+        invalid_candidates = []
+        
         for num_dict in nums_dict_list:
             if num_dict['value'] >= target:
-                print('Invalid candidate detected: ' + str(num_dict['value']))
+                invalid_candidates.append(num_dict)
+        
+        for candidate in invalid_candidates:
+            nums_dict_list.remove(candidate)
+        
+        new_list_element_count = len(nums_dict_list)
+        removed_item_count = original_list_element_count - new_list_element_count
         
 solution = Solution()
-solution.twoSum(nums = [1, 2, 3, 4, 5, 6, 7], target = 5)
+solution.twoSum(nums = [1, 2, 3, 4, 5, 6, 7], target = 6)
