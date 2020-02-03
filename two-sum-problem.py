766180cb4b9a6865493ebb6c936a7b32127ec8b9
viperior <viperior@gmail.com>
2020-02-03 02:03:03 +0000

    Add pythonic optimization of the original list of integers by removing candidates that cannot be valid operands.

--- two-sum-problem.py
+++ two-sum-problem.py
@@ -2,28 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict_list = []
-        current_num_index = 0
-        
-        for value in nums:
-            current_num_dict = {}
-            current_num_dict['index'] = current_num_index
-            current_num_dict['value'] = value
-            nums_dict_list.append(current_num_dict)
-            current_num_index += 1
-            
-        original_list_element_count = len(nums_dict_list)
-        invalid_candidates = []
-        
-        for num_dict in nums_dict_list:
-            if num_dict['value'] >= target:
-                invalid_candidates.append(num_dict)
-        
-        for candidate in invalid_candidates:
-            nums_dict_list.remove(candidate)
-        
-        new_list_element_count = len(nums_dict_list)
-        removed_item_count = original_list_element_count - new_list_element_count
+        for value in nums[:]:
+            if value >= target:
+                nums.remove(value)
+                
+        for value in enumerate(nums):
+            print(value)
         
 solution = Solution()
-solution.twoSum(nums = [1, 2, 3, 4, 5, 6, 7], target = 6)
+print(solution.twoSum(nums = [1, 2, 3, 4, 5, 6, 7], target = 6))
