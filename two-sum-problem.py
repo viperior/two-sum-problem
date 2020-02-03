eb06fa14baf1c17ac9f17e6ec92ed227a64faeb4
viperior <viperior@gmail.com>
2020-02-03 02:37:53 +0000

    Add solution that uses list slicing and index lookup. Solved!

--- two-sum-problem.py
+++ two-sum-problem.py
@@ -5,9 +5,14 @@ class Solution:
         for value in nums[:]:
             if value >= target:
                 nums.remove(value)
-                
-        for value in enumerate(nums):
-            print(value)
+        
+        for i, value in enumerate(nums):
+            operand_b_list = nums[:]
+            operand_b_list.remove(value)
+            
+            for j, operand_b in enumerate(operand_b_list):
+                if value + operand_b == target:
+                    return [nums.index(value), nums.index(operand_b)]
         
 solution = Solution()
 print(solution.twoSum(nums = [1, 2, 3, 4, 5, 6, 7], target = 6))
