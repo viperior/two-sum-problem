ee331786aa7bb7d555601ea75df5c3eb7777bb14
viperior <viperior@gmail.com>
2020-02-03 04:20:39 +0000

    Add handling for case when elements in input list repeat; truncate display of large input lists.

--- two-sum-problem.py
+++ two-sum-problem.py
@@ -11,23 +11,41 @@ class Solution:
                 operand_a_list.remove(value)
         
         
-        for i, value in enumerate(operand_a_list):
+        for i, operand_a in enumerate(operand_a_list):
             # Remove operand a from the list of operand b's to evaluate.
             operand_b_list = operand_a_list[:]
-            operand_b_list.remove(value)
+            operand_b_list.remove(operand_a)
             
             for j, operand_b in enumerate(operand_b_list):
-                if value + operand_b == target:
-                    solution_indices = [nums.index(value), nums.index(operand_b)]
+                if operand_a + operand_b == target:
+                    solution_indices = []
+                    operand_a_index = nums.index(operand_a)
+                    
+                    # Handle case where elements repeat in input array.
+                    if operand_a == operand_b:
+                        nums_list_without_operand_a = nums[:]
+                        del nums_list_without_operand_a[operand_a_index]
+                        operand_b_index = nums_list_without_operand_a.index(operand_b) + 1
+                    else:
+                        operand_b_index = nums.index(operand_b)
+                    
+                    solution_indices = [operand_a_index, operand_b_index]
                     self.display_solution(solution_indices, nums, target)
                     return solution_indices
                     
     def display_solution(self, solution_indices, nums, target):
-        print('List of integers: ' + str(nums))
+        print('List of integers:')
+        
+        # If input list is large, show only the first and last three elements.
+        if len(nums) > 20:
+            print(str(nums[0:3]) + '...' + str(nums[len(nums) - 4:len(nums) - 1]))
+        else:
+            print(str(nums))
+        
         print('Solution indices: ' + str(solution_indices))
         print(str(nums[solution_indices[0]]) + ' + ' + str(nums[solution_indices[1]]) + ' = ' + str(target))
         
 solution_a = Solution().two_sum(list(range(1, 6)), 3)
 solution_b = Solution().two_sum(list(range(1, 8)), 6)
 solution_c = Solution().two_sum([3, 3], 6)
-#solution_d = Solution().two_sum(list(range(8900, 100001, 21)), 93316)
+solution_d = Solution().two_sum(list(range(8900, 100001, 21)), 93316)
